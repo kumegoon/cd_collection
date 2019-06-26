e291849a3aeb37434eed3a9b5d71e148eaaa1b67
kumegoon <jiumi.jinjie@gmail.com>
2019-06-27 00:44:59 +0900

    devise done

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validates :nick_name, presence: true, length: { maximum: 100 }
+
+end
